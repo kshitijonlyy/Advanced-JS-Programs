58865c060d7f8d405f1909e8e3ebf5cbf19bd9ee
kshitijonlyy <114161430+kshitijonlyy@users.noreply.github.com>
2022-11-10 10:57:14 +0530

    Aync.js

--- async.js
+++ async.js
@@ -1,20 +1,20 @@
-async function myFun(){
-    console.log('start');
-    let myPromise = new Promise (function (resolve, reject){
+// async function myFun(){
+//     console.log('start');
+//     let myPromise = new Promise (function (resolve, reject){
 
-        var error = false;
+//         var error = false;
 
-        if (error==false){
-            console.log('resolved');
-            setTimeout(() => {resolve('by kshitij')}, 3000);
-        }
-        else{
-            console.log('rejected');
-            reject();
-        }
-    });
-    let result = await myPromise; // without await, the result will show <pending>
-    console.log(result);
-}
+//         if (error==false){
+//             console.log('resolved');
+//             setTimeout(() => {resolve('by kshitij')}, 3000);
+//         }
+//         else{
+//             console.log('rejected');
+//             reject();
+//         }
+//     });
+//     let result = await myPromise; // without await, the result will show <pending>
+//     console.log(result);
+// }
 
-myFun();
+// myFun();
